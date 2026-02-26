9d608728a282e5bbeb00d0225aefd7e273858925
Archanagiri <140953360+archana-giri2061@users.noreply.github.com>
2026-02-26 14:59:07 +0545

    All installization is complete and check json files

---
+++ Backend/src/app.ts
@@ -0,0 +1,25 @@
+//Import necessary libaries
+/**
+ * Express= node.js web framework which is used to create APIs, handle routes
+ * process requets and send responses
+ * 
+ * dotenv= load environment variables from .env file automatically
+ * 
+ */
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const port = Number(process.env.PORT);
+/*
+JSON= Data exchange between client and Server
+Frontend sends JSON- Backend reads JSON
+Backend sends JSON - frontend displays data 
+*/
+app.use(express.json());
+
+app.get("/Check", (_, res) => res.json({ "Details": "Everything is Good!!" }));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
