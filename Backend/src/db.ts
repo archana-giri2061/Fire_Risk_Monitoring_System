04b62dd7b1966c4c31b03dd767ae720b8265d848
Archanagiri <140953360+archana-giri2061@users.noreply.github.com>
2026-03-13 17:28:24 +0545

    database connection updated

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -2,10 +2,12 @@ import { Pool } from "pg";
 import "dotenv/config";
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
-export async function testDbConnection() {
-  const result = await pool.query("SELECT NOW() AS now");
-  return result.rows[0];
-}
