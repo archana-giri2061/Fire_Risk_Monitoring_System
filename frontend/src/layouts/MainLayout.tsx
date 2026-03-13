7c38384b828ce94648e2410ee16b8ff78bdebd68
Archanagiri <140953360+archana-giri2061@users.noreply.github.com>
2026-03-13 17:18:11 +0545

    update MainLayout

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,17 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
 
 export default function MainLayout() {
   return (
     <div className="app-shell">
-      <Sidebar />
-      <div className="main-area">
-        <Navbar />
-        <main className="page-content">
-          <Outlet />
-        </main>
-      </div>
+      <Navbar />
+      <main className="page-content">
+        <Outlet />
+      </main>
     </div>
   );
 }
