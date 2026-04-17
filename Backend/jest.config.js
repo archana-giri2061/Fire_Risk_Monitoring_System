05f21a45c3ee972571130260ca9cfabfc19511b7
Archanagiri <140953360+archana-giri2061@users.noreply.github.com>
2026-04-17 19:00:20 +0545

    Add jest.config.js with ts-jest transform

---
+++ Backend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  transform: { '^.+\\.tsx?$': 'ts-jest' },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  verbose: true,
+};
